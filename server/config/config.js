107160a54c1afb000bc9c9117a7fd8a05801d465
sabnq <sebastian.aranda@cityofeden.io>
2018-09-19 11:05:36 -0500

    Rest Server -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,23 @@
 //   PUERTO
 //========================
 process.env.PORT = process.env.PORT || 3000;
+
+//========================
+//   ENTORNO
+//========================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//========================
+//   BASES DE DATOS
+//========================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    //user = cafe-user
+    //password = 123456a
+    urlDB = 'mongodb://cafe-user:123456a@ds023550.mlab.com:23550/cafe';
+}
+
+process.env.URLDB = urlDB;
